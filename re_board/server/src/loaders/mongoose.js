5de0d83fe60679494282ba57cd6a7b6526d48285
unknown <sis000419@office.kw.ac.kr>
2022-05-08 17:38:41 +0900

    rm

--- re_board/server/src/loaders/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-async function mongooseLoader() {
-	const connetcion = 
-		  await mongoose.connect("mongodb+srv://inssong:seop@cluster0.5wcrw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-	return connetcion.connetcion;
-}
-exports.mongooseLoader = mongooseLoader;
